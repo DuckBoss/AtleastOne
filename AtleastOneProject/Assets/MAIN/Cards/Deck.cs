541f02be7fdfeecadb6e146612fcec17f9f0b73d
Jason [제이슨 제롬] <20238115+DuckBoss@users.noreply.github.com>
2020-04-25 01:22:26 -0400

    Updated documentation and added new scripts
    - Added Deck script to manage cards in a deck
    - Added doctype and meta tags to html files
    - Updated all documentation pages to support </> symbols

---
+++ AtleastOneProject/Assets/MAIN/Cards/Deck.cs
@@ -0,0 +1,62 @@
+﻿//!DOC
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using AtleastOne.Templates.Cards;
+using System.Linq;
+
+//!na
+namespace AtleastOne.Templates.Deck
+{
+    //!c
+    /*
+    Name: Deck
+    Description: The Deck class holds cards and handles various deck operations such as shuffling.
+    */
+    internal class Deck
+    {
+        private int _deckSize = 0;
+        protected internal int DeckSize
+        {
+            get { return _deckSize; }
+            set { _deckSize = value >= 0 ? value : 0; }
+        }
+
+        protected internal List<Card> Cards;
+
+        //!s
+        /*
+        Name: Deck
+        Description: The Deck constructur initializes the Deck class.
+        */
+        public Deck(int deckSize)
+        {
+            this.DeckSize = deckSize;
+        }
+        //#s
+
+        //!s
+        /*
+        Name: Shuffle
+        Description: Shuffles the cards in the deck randomly by generating new random Guid's and sorting them.
+        */
+        protected void Shuffle()
+        {
+            this.Cards = this.Cards.OrderBy(i => System.Guid.NewGuid()).ToList();
+        }
+        //#s
+
+        //!s
+        /*
+        Name: ToString
+        Description: Returns a string representation of the Deck class.
+        */
+        public override string ToString()
+        {
+            return $"Deck - " + string.Join(", ", this.Cards.ToArray<object>());
+        }
+        //#s
+    }
+    //#c
+}
+//#na
