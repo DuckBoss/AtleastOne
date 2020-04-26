b905171abe97f58f1a1d552e6b56c0cc5ce2d529
Jason [제이슨 제롬] <20238115+DuckBoss@users.noreply.github.com>
2020-04-26 01:00:47 -0400

    Update build.sh

--- Scripts/build.sh
+++ Scripts/build.sh
@@ -8,7 +8,7 @@ echo "Attempting to build $project for Windows"
   -force-free \
   -username "$U_USER" \
   -password "$U_PASS" \
-  -manualLicenceFile ./Unity_v2017.x.ulf \
+  -manualLicenseFile ./Unity_v2017.x.ulf \
   -nographics \
   -silent-crashes \
   -logFile $(pwd)/unity.log \
@@ -22,7 +22,7 @@ echo "Attempting to build $project for OS X"
   -force-free \
   -username "$U_USER" \
   -password "$U_PASS" \
-  -manualLicenceFile ./Unity_v2017.x.ulf \
+  -manualLicenseFile ./Unity_v2017.x.ulf \
   -nographics \
   -silent-crashes \
   -logFile $(pwd)/unity.log \
@@ -36,7 +36,7 @@ echo "Attempting to build $project for Linux"
   -force-free \
   -username "$U_USER" \
   -password "$U_PASS" \
-  -manualLicenceFile ./Unity_v2017.x.ulf \
+  -manualLicenseFile ./Unity_v2017.x.ulf \
   -nographics \
   -silent-crashes \
   -logFile $(pwd)/unity.log \
